48c151aba342bd4c5dc14ac5d6a0976c3b8f8252
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2021-04-28 23:06:48 +0530

    first commit

---
+++ lib/photo_detail_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class PhotoDetailPage extends StatelessWidget {
+  final String image;
+  final String tag;
+
+  const PhotoDetailPage({Key key, this.image, this.tag}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+backgroundColor: Colors.transparent,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: BackButton(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Center(
+              child: Hero(
+                tag: tag,
+                child: Image.network(
+                  image,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
